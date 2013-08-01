74b8f389844fd4fa2fc94a8c2779683d0a3628b0
Joe Buszkiewic <joe@buszkiewic.com>
2013-07-31 22:23:49 -0600

    Fixed View Test

--- spec/views/hipster/index.html.erb_spec.rb
+++ spec/views/hipster/index.html.erb_spec.rb
@@ -2,8 +2,7 @@ require "spec_helper"
 
 describe "hipster/index.html.erb" do
   it "should display a list of messages" do
-    messages = []
-    assign(:messages, 2.times { messages << FactoryGirl.create(:message) })
+    assign(:messages, Array.new(2) { FactoryGirl.build_stubbed(:message) })
 
     render
     rendered.should have_selector("#messages .message", :count => 2)
